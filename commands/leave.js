92ee070a0ed611b34bf2f02dd2bb0fbccb55f2bc
DevShimi92 <hospitalier.philippe@gmail.com>
2020-10-16 03:26:17 +0200

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -9,22 +9,42 @@ module.exports = {
 	usage: `${prefix}leave`,
 	execute(message) {
 
+		const serverQueue = message.client.queue.get(message.guild.id);
+
 		const { channel } = message.member.voice;
 
-		const serverQueue = message.client.queue.get(message.guild.id);
+		const channelBot = message.guild.voice.channel;
 
 		if (serverQueue) {
 
 			serverQueue.songs = [];
 
-			serverQueue.connection.dispatcher.end('Stop command has been used!');
+			serverQueue.connection.dispatcher.end('Leave command has been used!');
 
 		} 
 
 		message.client.queue.delete(message.guild.id);
 
-		channel.leave();
+		if (!channel) {
 
+				log.info(`L'utilisateur n'était pas dans le channel du bot lors de l'utilisation de la commande`);
+				channelBot.leave();
+
+			} 
+		else if (channel != null && channel != channelBot) {
+
+				log.info(`L'utilisateur n'était pas dans le même channel que celui du bot lors de l'utilisation de la commande`);
+				channelBot.leave();
+
+			}
+		else {
+
+				channel.leave();
+
+			}
+
+		
+		message.channel.send(`Bye ~ `);
 		log.info('Le bot est parti du channel');
 
 	},
